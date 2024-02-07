7de32a0e343d58d8ef525fb85c3cdd066458e841
JIY00N2 <angella990825@gmail.com>
2024-02-07 20:41:23 +0900

    feat: revalidate로 ISR 설정

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,12 @@
-import Link from "next/link";
 import { PostsService } from "./api/_lib/posts/service";
+
+export const revalidate = 30;
 
 export default async function HomePage() {
   const posts = await PostsService.getPosts();
 
   return (
     <main>
-      <Link href="/login">로그인</Link>
-      <Link href="/write">글 작성 페이지로 이동</Link>
       <div>
         {posts ? (
           posts.map((post) => (
