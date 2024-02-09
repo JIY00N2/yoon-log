81456cbee1ea60d098bca9714c04b2fad7891b8c
JIY00N2 <angella990825@gmail.com>
2024-02-09 17:42:09 +0900

    feat: 특정 포스트 페이지 구현 및 slug generateStaticParams로 SSG 구현

---
+++ app/posts/[slug]/page.tsx
@@ -0,0 +1,31 @@
+import { PostsService } from "@/app/api/_lib/posts/service";
+import AdminButton from "@/app/_components/AdminButton";
+
+export default async function PostPage({
+  params,
+}: {
+  params: { slug: string };
+}) {
+  const post = await PostsService.getPost(params.slug);
+  return (
+    <div>
+      <AdminButton slug={params.slug} />
+      {post ? (
+        <div>
+          <div>{post.title}</div>
+          <div>{post.content}</div>
+          <div>{post.createdAt.toLocaleString()}</div>
+        </div>
+      ) : (
+        <div>없음</div>
+      )}
+    </div>
+  );
+}
+
+export async function generateStaticParams() {
+  const posts = await PostsService.getPosts();
+  return posts.map((post) => ({
+    slug: post.slug,
+  }));
+}
