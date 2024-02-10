014667ba119cdb71afa8557811c167a30bf7aa73
JIY00N2 <angella990825@gmail.com>
2024-02-10 21:07:22 +0900

    refactor: ClientBoundary 타입 리팩터링

--- app/_components/ClientBoundary.tsx
+++ app/_components/ClientBoundary.tsx
@@ -1,12 +1,9 @@
 import { PropsWithChildren, ReactNode, useEffect, useState } from "react";
 
-type Props = {
-  fallback?: ReactNode;
-} & PropsWithChildren;
-
-// 첫 렌더링 시에는 useEffect 전이라 client, server 모두 isLogin이 false
-// useEffect를 쓰는 이유는 client와 server의 첫 렌더링을 같게하려고(아니면 오류발생) -> 클라이언트 바운더리 컴포넌트 만들자
-export default function ClientBoundary({ fallback, children }: Props) {
+export default function ClientBoundary({
+  fallback,
+  children,
+}: PropsWithChildren<{ fallback?: ReactNode }>) {
   const [isClient, setIsClient] = useState(false);
 
   useEffect(() => {
