a83338860f7b0515ab2734422c75563cfcdfc3c4
JIY00N2 <angella990825@gmail.com>
2024-02-10 21:12:31 +0900

    feat: 소개, 이력서 페이지 추가

---
+++ app/(root)/about/page.tsx
@@ -0,0 +1,7 @@
+import stylex from "@stylexjs/stylex";
+
+export default function AboutPage() {
+  return <div>소개 페이지</div>;
+}
+
+const styles = stylex.create({});
---
+++ app/(root)/resume/page.tsx
@@ -0,0 +1,7 @@
+import stylex from "@stylexjs/stylex";
+
+export default function ResumePage() {
+  return <div>이력서 페이지</div>;
+}
+
+const styles = stylex.create({});
