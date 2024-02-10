ba66ef822f54c78919848921fc43963a082ceade
JIY00N2 <angella990825@gmail.com>
2024-02-10 17:14:04 +0900

    feat: Footer 컴포넌트 임시 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import { Metadata } from "next";
 import "./globals.css";
 import Header from "./_components/Header";
+import Footer from "./_components/Footer";
 
 export const metadata: Metadata = {
   title: "Yoon's log",
@@ -17,6 +18,7 @@ export default function RootLayout({
       <body>
         <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
