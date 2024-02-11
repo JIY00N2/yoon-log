0cf84c68a276be47e4595d62d0e392cf5285dbf6
JIY00N2 <angella990825@gmail.com>
2024-02-11 15:45:13 +0900

    chore: stylex 버전 업데이트

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 
+const path = require("path");
 const stylexPlugin = require("@stylexjs/nextjs-plugin");
 
 module.exports = stylexPlugin({
+  aliases: {
+    "@/*": [path.join(__dirname, "*")],
+  },
   rootDir: __dirname,
 })({});
