8a2a362539bacefcb034aafb58260116328fda04
JIY00N2 <angella990825@gmail.com>
2024-02-15 19:35:33 +0900

    feat: 로딩중 처리 및 useCallback 사용

--- app/(root)/write/ImageForm.tsx
+++ app/(root)/write/ImageForm.tsx
@@ -1,35 +1,39 @@
 "use client";
 
 import { useContentContext } from "@/app/_context/ContentContext";
-import { ChangeEvent } from "react";
+import { ChangeEvent, useCallback } from "react";
 
 export default function ImageForm() {
-  const { setContent } = useContentContext();
-  async function handleInputChange(e: ChangeEvent<HTMLInputElement>) {
-    const formData = new FormData();
-    if (!e.target.files) {
-      return;
-    }
-    formData.append("file", e.target.files[0]);
-    const res = await fetch("/api/upload", {
-      method: "POST",
-      body: formData,
-    });
-    const data = (await res.json()) as {
-      imageFileName: string;
-      imageUrl: string;
-    };
-    let url = "![로딩중...]()";
-    if (data.imageUrl) {
-      url = `![${data.imageFileName}](${data.imageUrl})`;
-    }
-    setContent((content) => content + url);
-  }
+  const { setContent, isCompletedUploading } = useContentContext();
+  const handleInputChange = useCallback(
+    async (e: ChangeEvent<HTMLInputElement>) => {
+      const formData = new FormData();
+      if (!e.target.files) {
+        return;
+      }
+      formData.append("file", e.target.files[0]);
+      const res = await fetch("/api/upload", {
+        method: "POST",
+        body: formData,
+      });
+      const data = (await res.json()) as {
+        imageFileName: string;
+        imageUrl: string;
+      };
+      let url = "![로딩중...]()";
+      if (data.imageUrl) {
+        url = `![${data.imageFileName}](${data.imageUrl})`;
+      }
+      setContent((content) => content + url);
+    },
+    [setContent],
+  );
 
   return (
     <input
       type="file"
       onChange={handleInputChange}
+      disabled={isCompletedUploading}
     />
   );
 }
