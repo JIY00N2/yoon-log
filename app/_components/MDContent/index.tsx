989bdb0da95ff24209105600bbc35c0b8cafe96a
JIY00N2 <angella990825@gmail.com>
2024-02-17 15:08:22 +0900

    feat: dynamic import로 번들 크기 축소

--- app/_components/MDContent/index.tsx
+++ app/_components/MDContent/index.tsx
@@ -1,10 +1,14 @@
 "use client";
-import MDEditor from "@uiw/react-md-editor";
+import "@uiw/react-md-editor/markdown-editor.css";
+import "@uiw/react-markdown-preview/markdown.css";
+import dynamic from "next/dynamic";
+
+const Markdown = dynamic(() => import("@uiw/react-markdown-preview"));
 
 export default function MDContent({ source }: { source: string }) {
   return (
     <div data-color-mode="light">
-      <MDEditor.Markdown source={source} />
+      <Markdown source={source} />
     </div>
   );
 }
--- next.config.js
+++ next.config.js
@@ -2,19 +2,22 @@
 
 const path = require("path");
 const stylexPlugin = require("@stylexjs/nextjs-plugin");
+const removeImports = require("next-remove-imports")();
 
 module.exports = stylexPlugin({
   aliases: {
     "@/*": [path.join(__dirname, "*")],
   },
   rootDir: __dirname,
-})({
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "oxpdlbqcseajmjnxawbb.supabase.co",
-      },
-    ],
-  },
-});
+})(
+  removeImports({
+    images: {
+      remotePatterns: [
+        {
+          protocol: "https",
+          hostname: "oxpdlbqcseajmjnxawbb.supabase.co",
+        },
+      ],
+    },
+  }),
+);
