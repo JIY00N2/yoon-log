15fd980959dd1e979730bd701fcfdd4c9182abdd
JIY00N2 <angella990825@gmail.com>
2024-02-17 17:23:21 +0900

    style: 포스트 상세 페이지 스타일링

--- app/(root)/posts/[slug]/page.tsx
+++ app/(root)/posts/[slug]/page.tsx
@@ -1,7 +1,10 @@
+import stylex from "@stylexjs/stylex";
+import Image from "next/image";
 import AdminButton from "@/app/_components/AdminButton";
 import MDContent from "@/app/_components/MDContent";
 import { PostsService } from "@/app/_lib/posts/service";
 import formattedDate from "@/app/_utils/formattedDate";
+import { colors } from "@/app/tokens.stylex";
 
 export default async function PostPage({
   params,
@@ -11,16 +14,39 @@ export default async function PostPage({
   const post = await PostsService.getPost(decodeURIComponent(params.slug));
 
   return (
-    <div>
-      <AdminButton slug={params.slug} />
+    <div {...stylex.props(styles.layout)}>
       {post ? (
-        <div>
-          <div>{post.title}</div>
-          <div>createdAt: {formattedDate(post.createdAt)}</div>
-          <MDContent source={post.content} />
+        <div {...stylex.props(styles.container)}>
+          <section {...stylex.props(styles.titleContainer)}>
+            <h1 {...stylex.props(styles.title)}>{post.title}</h1>
+            <div {...stylex.props(styles.infos)}>
+              <div {...stylex.props(styles.info)}>
+                <span {...stylex.props(styles.author)}>By Yoon</span>
+                <div {...stylex.props(styles.author)}>·</div>
+                <time {...stylex.props(styles.time)}>
+                  {formattedDate(post.createdAt)}
+                </time>
+              </div>
+              <AdminButton slug={params.slug} />
+            </div>
+          </section>
+          <section {...stylex.props(styles.thumbnail)}>
+            {post.thumbnailUrl && (
+              <Image
+                src={post.thumbnailUrl}
+                alt="thumbnail"
+                priority
+                layout="fill"
+                objectFit="cover"
+              />
+            )}
+          </section>
+          <section {...stylex.props(styles.content)}>
+            <MDContent source={post.content} />
+          </section>
         </div>
       ) : (
-        <div>없음</div>
+        <span>없음</span>
       )}
     </div>
   );
@@ -32,3 +58,63 @@ export async function generateStaticParams() {
     slug: post.slug,
   }));
 }
+
+const styles = stylex.create({
+  layout: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    width: "100%",
+    height: "100%",
+  },
+  container: {
+    display: "flex",
+    flexDirection: "column",
+    position: "relative",
+    width: "100%",
+    maxWidth: "768px",
+    gap: "2rem",
+  },
+  titleContainer: {
+    display: "flex",
+    flexDirection: "column",
+    gap: "2.25rem",
+  },
+  title: {
+    fontSize: "2rem",
+    fontWeight: 700,
+    color: colors.black,
+  },
+  infos: {
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  info: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    gap: "0.5rem",
+  },
+  author: {
+    fontSize: "1.2rem",
+    fontWeight: 700,
+    color: colors.black,
+  },
+  time: {
+    textAlign: "center",
+    fontSize: "1rem",
+    fontWeight: 600,
+    color: colors.black,
+  },
+  thumbnail: {
+    display: "flex",
+    width: "100%",
+    minHeight: "420px",
+    position: "relative",
+    borderRadius: "10px",
+    overflow: "hidden",
+  },
+  content: {},
+});
