77780e59299a51e89de8e3a749fdcb839b7223ea
JIY00N2 <angella990825@gmail.com>
2024-02-20 02:47:57 +0900

    feat: 포스트 삭제가 안되는 현상 수정
    route handler로 홈과 삭제하려는 포스트 경로를 revalidate하는 로직

--- app/_components/AdminButton/index.tsx
+++ app/_components/AdminButton/index.tsx
@@ -1,19 +1,35 @@
 "use client";
-import { useRouter } from "next/navigation";
 import Link from "next/link";
 import stylex from "@stylexjs/stylex";
 import ClientBoundary from "../ClientBoundary";
 import isLoggedIn from "@/app/_utils/isLoggedIn";
+import { useRouter } from "next/navigation";
 
 export default function AdminButton({ slug }: { slug: string }) {
   const router = useRouter();
-
   async function handleDeleteClick() {
     await fetch(`/api/posts/${slug}`, {
       method: "DELETE",
     });
-    router.replace("/");
-    router.refresh();
+    const [resHome, resPost] = await Promise.all([
+      fetch("/api/revalidate?path=/"),
+      fetch(`/api/revalidate?path=/api/posts/${slug}`),
+    ]);
+    const home = (await resHome.json()) as {
+      revalidated: boolean;
+      message?: string;
+    };
+    const post = (await resPost.json()) as {
+      revalidated: boolean;
+      message?: string;
+    };
+    if (home.revalidated && post.revalidated) {
+      router.replace("/");
+      router.refresh();
+    } else {
+      console.log(home.message, post.message);
+      // TODO: 토스트 ui
+    }
   }
 
   return (
