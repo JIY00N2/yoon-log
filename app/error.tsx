10bd2182d4e897730c9162dd02ee1dbd043a2131
JIY00N2 <angella990825@gmail.com>
2024-02-27 19:40:08 +0900

    feat: 에러 페이지 추가 (#9)

---
+++ app/error.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import { useEffect } from "react";
+import stylex from "@stylexjs/stylex";
+
+export default function GlobalError({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: VoidFunction;
+}) {
+  useEffect(() => {
+    console.error(error);
+  }, [error]);
+
+  return (
+    <section {...stylex.props(styles.layout)}>
+      <h1>문제가 발생했습니다.</h1>
+      <button
+        onClick={() => reset()}
+        {...stylex.props(styles.button)}
+      >
+        재시도
+      </button>
+    </section>
+  );
+}
+
+const styles = stylex.create({
+  layout: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: "180px",
+    gap: "0.5rem",
+  },
+  button: {
+    marginTop: "20px",
+    paddingHorizontal: "1rem",
+    paddingVertical: "0.5rem",
+    borderRadius: "0.3rem",
+    backgroundColor: "rgb(3, 152, 178)",
+    fontSize: "1.2rem",
+    fontWeight: 700,
+    color: "white",
+  },
+});
