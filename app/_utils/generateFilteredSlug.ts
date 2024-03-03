df07b4884db3f80cff48f72976c7056879b6a11e
JIY00N2 <angella990825@gmail.com>
2024-03-03 20:10:26 +0900

    feat: slug에 숫자도 허용하도록 수정

--- app/_utils/generateFilteredSlug.ts
+++ app/_utils/generateFilteredSlug.ts
@@ -1,6 +1,6 @@
 export default function generateFilteredSlug(slug: string) {
   return slug
-    .replace(/[^a-zA-Z\s]/g, "")
+    .replace(/[^a-zA-Z0-9\s]/g, "")
     .trim()
     .toLocaleLowerCase()
     .replace(/\s+/g, "-");
