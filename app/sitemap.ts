99d2a6c349e158b2f7239ba4c3550d5f0e0aa6ee
JIY00N2 <angella990825@gmail.com>
2024-03-06 18:50:54 +0900

    fix: sitemap.xml 안되는 현상 테스트 (#16)

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,26 +3,34 @@ import formattedDate from "./_utils/formattedDate";
 import { PostsService } from "./_lib/posts/service";
 import { BASE_URL } from "./_constants";
 
-export async function generateSitemaps() {
-  const posts = await PostsService.getPosts();
-  return posts.map((post) => ({
-    id: post._id,
-  }));
-}
+// export async function generateSitemaps() {
+//   const posts = await PostsService.getPosts();
+//   return posts.map((post) => ({
+//     id: post._id,
+//   }));
+// }
 
-export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+export default function sitemap(): MetadataRoute.Sitemap {
   const routesSitemaps = ["/", "/about", "/portfolio"].map((route) => ({
     url: `${BASE_URL}${route}`,
     lastModified: formattedDate(new Date()),
   }));
-
-  const posts = await PostsService.getPosts();
-  const postsSitemaps = posts.map((post) => ({
-    url: `${BASE_URL}/posts/${post.slug}`,
-    lastModified: formattedDate(post.updatedAt),
-  }));
-
-  const sitemaps = [...routesSitemaps, ...postsSitemaps];
-
-  return sitemaps;
+  return routesSitemaps;
 }
+
+// export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+//   const routesSitemaps = ["/", "/about", "/portfolio"].map((route) => ({
+//     url: `${BASE_URL}${route}`,
+//     lastModified: formattedDate(new Date()),
+//   }));
+
+//   const posts = await PostsService.getPosts();
+//   const postsSitemaps = posts.map((post) => ({
+//     url: `${BASE_URL}/posts/${post.slug}`,
+//     lastModified: formattedDate(post.updatedAt),
+//   }));
+
+//   const sitemaps = [...routesSitemaps, ...postsSitemaps];
+
+//   return sitemaps;
+// }
