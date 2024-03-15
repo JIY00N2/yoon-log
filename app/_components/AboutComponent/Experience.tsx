e40e8dbde46d59d892bbbef2d3376d1da3a2ab1b
JIY00N2 <angella990825@gmail.com>
2024-03-15 09:47:02 +0900

    chore: Experience 수정 (#10)

--- app/_components/AboutComponent/Experience.tsx
+++ app/_components/AboutComponent/Experience.tsx
@@ -3,9 +3,8 @@ import stylex from "@stylexjs/stylex";
 export default function Experience() {
   return (
     <div {...stylex.props(styles.layout)}>
-      <p>- 프로그래머스 데브코스 프런트엔드 4기 수료 (2023.06 ~ 2023.12)</p>
-      <p>- 광운대학교 전자통신공학과 졸업 (2021.03 ~ 2023.02)</p>
-      <p>- 홍익대학교 세종캠퍼스 소프트웨어융합학과 자퇴 (2018.03 ~ 2020.02)</p>
+      <p>- 프로그래머스 데브코스 프런트엔드 4기 수료</p>
+      <p>- 광운대학교 전자통신공학과 졸업</p>
     </div>
   );
 }
