39093f05df8b94650eee017a3517a73c5b0ca05e
JIY00N2 <angella990825@gmail.com>
2024-04-10 17:52:16 +0900

    feat: dynamic 경로 sitemap 추가 (#22)

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,14 @@
 import { MetadataRoute } from "next";
 import { BASE_URL } from "./_constants";
+import { PostsService } from "./_lib/posts/service";
 
-export default function sitemap(): MetadataRoute.Sitemap {
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const posts = await PostsService.getPosts();
+  const postsSitemap = posts.map((post) => ({
+    url: `${BASE_URL}/posts/${post.slug}`,
+    lastModified: new Date(),
+  }));
+
   return [
     {
       url: BASE_URL,
@@ -11,5 +18,6 @@ export default function sitemap(): MetadataRoute.Sitemap {
       url: `${BASE_URL}/about`,
       lastModified: new Date(),
     },
+    ...postsSitemap,
   ];
 }
