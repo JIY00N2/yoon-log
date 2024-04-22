aeea87066c909eb2dc0f95584218fdf8213025bb
JIY00N2 <angella990825@gmail.com>
2024-04-22 21:20:15 +0900

    fix: utc를 kst로 변경

--- app/_utils/formattedDate.ts
+++ app/_utils/formattedDate.ts
@@ -1,3 +1,6 @@
 export default function formattedDate(date: NativeDate) {
-  return date.toISOString().split("T")[0];
+  const gmtPlus9h = new Date(
+    Number(new Date(date)) + 1000 * 60 * 60 * 9,
+  ).toISOString();
+  return gmtPlus9h.split("T")[0];
 }
